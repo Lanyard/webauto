bf6ec3ff3ccece3e094156200c5124c29caf14fc
Raynald Mirville <raynald.mirville@gmail.com>
2013-11-14 11:20:27 -0500

    Attempting new merge

---
+++ mod/chat/index.php
@@ -0,0 +1,13 @@
+
+<html>
+<head>
+	<script type="text/javascript" src="/si664/includes/jquery-1.10.2.min.js" />
+</head>
+<body>
+	<h1>Chatroom</h1>
+	<form method="post" action="index.php">
+		<input type="text" name="message" size="60" />
+		<input type="submit" value="Send Message" />
+	</form>
+</body>
+</html>
